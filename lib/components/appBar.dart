70bb415d7b5bb5de844f965e65eb0e9d73c67076
UweKnopf <69891951+UweKnopf@users.noreply.github.com>
2023-06-09 21:40:56 +0200

    Combined the two Appbars into one + statefull

--- lib/components/appBar.dart
+++ lib/components/appBar.dart
@@ -16,7 +16,7 @@ class AppBarVSCO extends StatefulWidget {
 class _AppBarVSCOState extends State<AppBarVSCO> {
   bool logedIn = false;
 
-  refresh() {
+  updateLoginState() {
     setState(() {
       logedIn = !logedIn;
     });
@@ -24,26 +24,36 @@ class _AppBarVSCOState extends State<AppBarVSCO> {
 
   @override
   Widget build(BuildContext context) {
-    return logedIn
-        ? const AppBarSignedIn()
-        : AppBarSignedOut(
-            notifyParent: refresh,
-          );
+    return AppBar(
+      notifyParent: updateLoginState,
+      logedIn: logedIn,
+    );
   }
 }
 
-class AppBarSignedOut extends StatefulWidget {
+class AppBar extends StatefulWidget {
   final Function() notifyParent;
-  const AppBarSignedOut({
+  final bool logedIn;
+  const AppBar({
     super.key,
     required this.notifyParent,
+    required this.logedIn,
   });
-
+  //bool logedIn;
   @override
-  State<AppBarSignedOut> createState() => _AppBarSignedOutState();
+  State<AppBar> createState() => _AppBarState();
 }
 
-class _AppBarSignedOutState extends State<AppBarSignedOut> {
+class _AppBarState extends State<AppBar> {
+  String pageButtonSelected =
+      'Features'; //Features, Plans, Whats New, Feed and Profile
+
+  updateButtonSelectedState(String ppageButtonSelected) {
+    setState(() {
+      pageButtonSelected = ppageButtonSelected;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -76,68 +86,10 @@ class _AppBarSignedOutState extends State<AppBarSignedOut> {
                 width: 40,
               ),
               TextButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  overlayColor: MaterialStateProperty.all(Colors.transparent),
-                  foregroundColor: MaterialStateProperty.resolveWith<Color>(
-                      (Set<MaterialState> states) {
-                    if (states.contains(MaterialState.hovered)) {
-                      return const Color(0xFF737373);
-                    }
-                    return Colors.black;
-                  }),
-                ),
-                child: const Text(
-                  'Features',
-                  style: TextStyle(fontSize: 17),
-                ),
-              ),
-              const SizedBox(
-                width: 40,
-              ),
-              TextButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  overlayColor: MaterialStateProperty.all(Colors.transparent),
-                  foregroundColor: MaterialStateProperty.resolveWith<Color>(
-                      (Set<MaterialState> states) {
-                    if (states.contains(MaterialState.hovered)) {
-                      return const Color(0xFF737373);
-                    }
-                    return Colors.black;
-                  }),
-                ),
-                child: const Text(
-                  'Plans',
-                  style: TextStyle(fontSize: 17),
-                ),
-              ),
-              const SizedBox(
-                width: 40,
-              ),
-              TextButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  overlayColor: MaterialStateProperty.all(Colors.transparent),
-                  foregroundColor: MaterialStateProperty.resolveWith<Color>(
-                      (Set<MaterialState> states) {
-                    if (states.contains(MaterialState.hovered)) {
-                      return const Color(0xFF737373);
-                    }
-                    return Colors.black;
-                  }),
-                ),
-                child: const Text(
-                  'What\'s New',
-                  style: TextStyle(fontSize: 17),
-                ),
-              ),
-              const SizedBox(
-                width: 40,
-              ),
-              TextButton(
                 onPressed: () {
-                  widget.notifyParent();
+                  setState(() {
+                    pageButtonSelected = 'Feature';
+                  });
                 },
                 style: ButtonStyle(
                   overlayColor: MaterialStateProperty.all(Colors.transparent),
@@ -149,135 +101,25 @@ class _AppBarSignedOutState extends State<AppBarSignedOut> {
                     return Colors.black;
                   }),
                 ),
-                child: const Text(
-                  'Sign in',
-                  style: TextStyle(fontSize: 17),
-                ),
-              ),
-              const SizedBox(
-                width: 40,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                    overlayColor: MaterialStateProperty.all(Colors.transparent),
-                    backgroundColor:
-                        MaterialStateProperty.all(const Color(0xFF0000FF)),
-                    foregroundColor: MaterialStateProperty.resolveWith<Color>(
-                        (Set<MaterialState> states) {
-                      return Colors.white;
-                    }),
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16),
-                      ),
-                    )),
-                child: const Text(
-                  'Download VSCO',
-                  style: TextStyle(fontSize: 15),
-                ),
-              ),
-              const SizedBox(
-                width: 40,
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              TextButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  overlayColor: MaterialStateProperty.all(Colors.transparent),
-                  foregroundColor: MaterialStateProperty.resolveWith<Color>(
-                      (Set<MaterialState> states) {
-                    if (states.contains(MaterialState.hovered)) {
-                      return const Color(0xFF737373);
-                    }
-                    return Colors.black;
-                  }),
-                ),
-                child: const Icon(Icons.search),
-              ),
-              TextButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  overlayColor: MaterialStateProperty.all(Colors.transparent),
-                  foregroundColor: MaterialStateProperty.resolveWith<Color>(
-                      (Set<MaterialState> states) {
-                    return Colors.black;
-                  }),
-                ),
-                child: const Icon(Icons.menu_outlined),
-              ),
-              const SizedBox(
-                width: 40,
-              ),
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class AppBarSignedIn extends StatelessWidget {
-  const AppBarSignedIn({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 85,
-      decoration: const BoxDecoration(
-        color: Colors.white,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              const SizedBox(
-                width: 40,
-              ),
-              TextButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                    overlayColor: MaterialStateProperty.all(Colors.transparent),
-                    foregroundColor: MaterialStateProperty.resolveWith<Color>(
-                        (Set<MaterialState> states) {
-                      if (states.contains(MaterialState.hovered)) {
-                        return const Color.fromARGB(255, 165, 165, 165);
-                      }
-                      return Colors.black;
-                    }),
-                  ),
-                  child: SvgPicture.asset('/images/VSCO-logo.svg')),
-              const SizedBox(
-                width: 40,
-              ),
-              TextButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  overlayColor: MaterialStateProperty.all(Colors.transparent),
-                  foregroundColor: MaterialStateProperty.resolveWith<Color>(
-                      (Set<MaterialState> states) {
-                    if (states.contains(MaterialState.hovered)) {
-                      return const Color(0xFF737373);
-                    }
-                    return Colors.black;
-                  }),
-                ),
-                child: const Text(
+                child: Text(
                   'Features',
-                  style: TextStyle(fontSize: 17),
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: pageButtonSelected == 'Feature'
+                        ? FontWeight.bold
+                        : FontWeight.normal,
+                  ),
                 ),
               ),
               const SizedBox(
                 width: 40,
               ),
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    pageButtonSelected = 'Plans';
+                  });
+                },
                 style: ButtonStyle(
                   overlayColor: MaterialStateProperty.all(Colors.transparent),
                   foregroundColor: MaterialStateProperty.resolveWith<Color>(
@@ -288,16 +130,25 @@ class AppBarSignedIn extends StatelessWidget {
                     return Colors.black;
                   }),
                 ),
-                child: const Text(
+                child: Text(
                   'Plans',
-                  style: TextStyle(fontSize: 17),
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: pageButtonSelected == 'Plans'
+                        ? FontWeight.bold
+                        : FontWeight.normal,
+                  ),
                 ),
               ),
               const SizedBox(
                 width: 40,
               ),
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    pageButtonSelected = 'What\'s New';
+                  });
+                },
                 style: ButtonStyle(
                   overlayColor: MaterialStateProperty.all(Colors.transparent),
                   foregroundColor: MaterialStateProperty.resolveWith<Color>(
@@ -308,76 +159,146 @@ class AppBarSignedIn extends StatelessWidget {
                     return Colors.black;
                   }),
                 ),
-                child: const Text(
+                child: Text(
                   'What\'s New',
-                  style: TextStyle(fontSize: 17),
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: pageButtonSelected == 'What\'s New'
+                        ? FontWeight.bold
+                        : FontWeight.normal,
+                  ),
                 ),
               ),
               const SizedBox(
                 width: 40,
               ),
-              TextButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  overlayColor: MaterialStateProperty.all(Colors.transparent),
-                  foregroundColor: MaterialStateProperty.resolveWith<Color>(
-                      (Set<MaterialState> states) {
-                    if (states.contains(MaterialState.hovered)) {
-                      return const Color(0xFF737373);
-                    }
-                    return Colors.black;
-                  }),
-                ),
-                child: const Text(
-                  'Feed',
-                  style: TextStyle(fontSize: 17),
-                ),
-              ),
-              const SizedBox(
-                width: 40,
-              ),
-              TextButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  overlayColor: MaterialStateProperty.all(Colors.transparent),
-                  foregroundColor: MaterialStateProperty.resolveWith<Color>(
-                      (Set<MaterialState> states) {
-                    if (states.contains(MaterialState.hovered)) {
-                      return const Color(0xFF737373);
-                    }
-                    return Colors.black;
-                  }),
-                ),
-                child: const Text(
-                  'Profile',
-                  style: TextStyle(fontSize: 17),
-                ),
-              ),
-              const SizedBox(
-                width: 40,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                    overlayColor: MaterialStateProperty.all(Colors.transparent),
-                    backgroundColor: MaterialStateProperty.all(Colors.black),
-                    foregroundColor: MaterialStateProperty.resolveWith<Color>(
-                        (Set<MaterialState> states) {
-                      return Colors.white;
-                    }),
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16),
-                      ),
-                    )),
-                child: const Text(
-                  'Upgrade',
-                  style: TextStyle(fontSize: 15),
-                ),
-              ),
-              const SizedBox(
-                width: 40,
-              ),
+              widget.logedIn
+                  ? Row(
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            overlayColor:
+                                MaterialStateProperty.all(Colors.transparent),
+                            foregroundColor:
+                                MaterialStateProperty.resolveWith<Color>(
+                                    (Set<MaterialState> states) {
+                              if (states.contains(MaterialState.hovered)) {
+                                return const Color(0xFF737373);
+                              }
+                              return Colors.black;
+                            }),
+                          ),
+                          child: const Text(
+                            'Feed',
+                            style: TextStyle(fontSize: 17),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 40,
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            overlayColor:
+                                MaterialStateProperty.all(Colors.transparent),
+                            foregroundColor:
+                                MaterialStateProperty.resolveWith<Color>(
+                                    (Set<MaterialState> states) {
+                              if (states.contains(MaterialState.hovered)) {
+                                return const Color(0xFF737373);
+                              }
+                              return Colors.black;
+                            }),
+                          ),
+                          child: const Text(
+                            'Profile',
+                            style: TextStyle(fontSize: 17),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 40,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                              overlayColor:
+                                  MaterialStateProperty.all(Colors.transparent),
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.black),
+                              foregroundColor:
+                                  MaterialStateProperty.resolveWith<Color>(
+                                      (Set<MaterialState> states) {
+                                return Colors.white;
+                              }),
+                              shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                              )),
+                          child: const Text(
+                            'Upgrade',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 40,
+                        ),
+                      ],
+                    )
+                  : Row(
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            widget.notifyParent();
+                          },
+                          style: ButtonStyle(
+                            overlayColor:
+                                MaterialStateProperty.all(Colors.transparent),
+                            foregroundColor:
+                                MaterialStateProperty.resolveWith<Color>(
+                                    (Set<MaterialState> states) {
+                              if (states.contains(MaterialState.hovered)) {
+                                return const Color(0xFF737373);
+                              }
+                              return Colors.black;
+                            }),
+                          ),
+                          child: const Text(
+                            'Sign in',
+                            style: TextStyle(fontSize: 17),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 40,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                              overlayColor:
+                                  MaterialStateProperty.all(Colors.transparent),
+                              backgroundColor: MaterialStateProperty.all(
+                                  const Color(0xFF0000FF)),
+                              foregroundColor:
+                                  MaterialStateProperty.resolveWith<Color>(
+                                      (Set<MaterialState> states) {
+                                return Colors.white;
+                              }),
+                              shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                              )),
+                          child: const Text(
+                            'Download VSCO',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 40,
+                        ),
+                      ],
+                    ),
             ],
           ),
           Row(
